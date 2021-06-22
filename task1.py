1517a796a103c5914a0ff996c681f025350f4b34
andr_psh <andr_psh@ukr.net>
2021-06-22 15:39:05 +0300

    up1

--- task1.py
+++ task1.py
@@ -1,1 +1,21 @@
-111
+
+a = input()
+num_list = []
+
+num = ''
+for char in a:
+    if char.isdigit():
+        num = num + char
+    else:
+        if num != '':
+            num_list.append(int(num))
+            num = ''
+if num != '':
+    num_list.append(int(num))
+    a1 = "".join(c for c in a if c.isalpha())
+    print(a1)
+    print(num_list)
+
+
+
+
