2b1cae4440568e51011aadaffd9148d6976166e1
andr_psh <andr_psh@ukr.net>
2021-06-22 23:45:03 +0300

    up2Final

--- task1.py
+++ task1.py
@@ -13,8 +13,22 @@ for char in a:
 if num != '':
     num_list.append(int(num))
     a1 = "".join(c for c in a if c.isalpha())
+
+
     print(a1)
     print(num_list)
+    a1 = a1.capitalize()
+    a1 = a1[:-1] + a1[-1].upper()
+    print(a1)
+    print(max(num_list))
+    num_listWOL = num_list
+    num_list.remove(max(num_listWOL))
+    print(num_listWOL)
+    num_listWOLnew = [i * i for i in num_listWOL]
+    print (num_listWOLnew)
+
+
+
 
 
 
